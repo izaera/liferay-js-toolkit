d848bf26d3c62e16b78af753c6cd22d7e457777c
Iván Zaera Avellón <izaera@gmx.es>
2019-09-26 12:45:36 +0200

    Fixes #419: Define global types

---
+++ packages/liferay-npm-build-tools-common/src/types.d.ts
@@ -0,0 +1,11 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+/**
+ * A glob expression that can be used with the `globby` package (extended glob
+ * syntax, including **, using POSIX format for paths).
+ */
+type glob = string;
